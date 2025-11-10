9c9479bfd61f7f100a4df37d3229d3277e9a6eeb
BelAnouar <anwarbelhassan@example.com>
2025-11-10 23:58:26 +0100

    Add FxDealController for handling FX deal creation requests

---
+++ src/main/java/org/hotelreservation/clustereddatawarehouse/controller/FxDealController.java
@@ -0,0 +1,27 @@
+package org.hotelreservation.clustereddatawarehouse.controller;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hotelreservation.clustereddatawarehouse.model.dto.FxDealRequest;
+import org.hotelreservation.clustereddatawarehouse.model.dto.FxDealResponse;
+import org.hotelreservation.clustereddatawarehouse.service.FxDealService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/deals")
+@RequiredArgsConstructor
+@Slf4j
+public class FxDealController {
+  private final FxDealService fxDealService;
+   @PostMapping
+   @ResponseStatus(HttpStatus.CREATED)
+   public FxDealResponse createDeal(@Valid @RequestBody FxDealRequest request) {
+       log.info("Received request to create FX deal: {}", request.dealUniqueId());
+       return fxDealService.createDeal(request);
+   }
+
+}
