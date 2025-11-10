72f9ed53a7231d69cd44ca558a69dbf93bb7a861
BelAnouar <anwarbelhassan@example.com>
2025-11-10 23:06:26 +0100

    Add FxDealRepository interface for managing FxDeal entities

---
+++ src/main/java/org/hotelreservation/clustereddatawarehouse/repository/FxDealRepository.java
@@ -0,0 +1,11 @@
+package org.hotelreservation.clustereddatawarehouse.repository;
+
+
+import org.hotelreservation.clustereddatawarehouse.model.entity.FxDeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FxDealRepository extends JpaRepository<FxDeal, String> {
+   
+}
