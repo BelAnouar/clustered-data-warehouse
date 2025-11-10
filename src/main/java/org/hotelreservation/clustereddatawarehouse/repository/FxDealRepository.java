06e5c14106851650cf8ad19b491d8dc1939b1c2d
BelAnouar <anwarbelhassan@example.com>
2025-11-10 23:43:42 +0100

    Add existsByDealUniqueId method to FxDealRepository for checking unique FX deal IDs

--- src/main/java/org/hotelreservation/clustereddatawarehouse/repository/FxDealRepository.java
+++ src/main/java/org/hotelreservation/clustereddatawarehouse/repository/FxDealRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FxDealRepository extends JpaRepository<FxDeal, String> {
-   
+    boolean existsByDealUniqueId(String s);
 }
