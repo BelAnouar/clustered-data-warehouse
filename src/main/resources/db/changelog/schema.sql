31f00bf89407fa29fdcfc521f51260391a9a0388
BelAnouar <anwarbelhassan@example.com>
2025-11-10 20:31:09 +0100

    Update .gitignore to include /data directory and modify schema.sql for deal table structure

--- src/main/resources/db/changelog/schema.sql
+++ src/main/resources/db/changelog/schema.sql
@@ -1,11 +1,8 @@
-CREATE DATABASE fx_deals;
-
-
 CREATE TABLE IF NOT EXISTS deals (
-                                     deal_id VARCHAR(50) PRIMARY KEY,
-    from_currency_iso VARCHAR(3) NOT NULL,
-    to_currency_iso VARCHAR(3) NOT NULL,
+                                     deal_unique_id VARCHAR(50) PRIMARY KEY,
+    from_currency_iso_code VARCHAR(3) NOT NULL,
+    to_currency_iso_code VARCHAR(3) NOT NULL,
     deal_timestamp TIMESTAMP NOT NULL,
     deal_amount DECIMAL(18,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    );
